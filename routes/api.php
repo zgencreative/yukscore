6ff91afe6deb94ced6145327804a066683bada75
zgencreative <zgencreative@gmail.com>
2026-04-17 09:21:36 +0700

    feat: migrate football API routes from web.php to api.php and register them in application bootstrap

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,30 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/football/favorit', [ApiController::class, 'getMatchFavorite']);
+Route::get('/football/detailCountry/{country}', [ApiController::class, 'detailCountry']);
+    Route::post('/football/favorit', [ApiController::class, 'getMatchFavorite']);
+    Route::get('/football/{date}', [ApiController::class, 'footballByDate']);
+    Route::get('/sorted-data/{date?}', [ApiController::class, 'sortedData']);
+    Route::get('/search/{keyword?}/{type?}', [ApiController::class, 'search']);
+
+    Route::prefix('football/detailMatch')->group(function () {
+        Route::get('/{idMatch}', [ApiController::class, 'getDetailMatch']);
+        Route::get('/stat/{idMatch}', [ApiController::class, 'getMatchStat']);
+        Route::get('/lineup/{idMatch}', [ApiController::class, 'getMatchLineup']);
+        Route::get('/table/{urlComp}', [ApiController::class, 'getLeagueTable']);
+        Route::get('/news/{idMatch}', [ApiController::class, 'getMatchNews']);
+        Route::get('/info/{id}', [ApiController::class, 'getDetailMatchInfo']);
+    });
+
+    Route::prefix('football/detailComp')->group(function () {
+        Route::get('/{urlComp}', [ApiController::class, 'getCompetitionDetail']);
+    });
+
+    Route::prefix('football/detailTeam')->group(function () {
+        Route::get('/{idTeam}', [ApiController::class, 'getTeamDetail']);
+        Route::get('/playerstat/{teamId}/{eventId?}', [ApiController::class, 'getPlayerStat']);
+        Route::get('/squad/{id}', [ApiController::class, 'getTeamSquad']);
+        Route::get('/stat/{teamId}/{eventId?}', [ApiController::class, 'getTeamStat']);
+        Route::get('/table/{id}', [ApiController::class, 'getTeamTable']);
+        Route::get('/news/{idTeam}', [ApiController::class, 'getTeamNews']);
+    });
--- routes/web.php
+++ routes/web.php
@@ -29,35 +29,9 @@ Route::get('/mobile-stream', [MobileStreamController::class, 'showIframe']);
 
 //BACKEND API
 
-Route::prefix('api')->group(function () {
-    Route::get('/football/detailCountry/{country}', [ApiController::class, 'detailCountry']);
-    Route::post('/football/favorite', [ApiController::class, 'getMatchFavorite']);
-    Route::get('/football/{date}', [ApiController::class, 'footballByDate']);
-    Route::get('/sorted-data/{date?}', [ApiController::class, 'sortedData']);
-    Route::get('/search/{keyword?}/{type?}', [ApiController::class, 'search']);
-
-    Route::prefix('football/detailMatch')->group(function () {
-        Route::get('/{idMatch}', [ApiController::class, 'getDetailMatch']);
-        Route::get('/stat/{idMatch}', [ApiController::class, 'getMatchStat']);
-        Route::get('/lineup/{idMatch}', [ApiController::class, 'getMatchLineup']);
-        Route::get('/table/{urlComp}', [ApiController::class, 'getLeagueTable']);
-        Route::get('/news/{idMatch}', [ApiController::class, 'getMatchNews']);
-        Route::get('/info/{id}', [ApiController::class, 'getDetailMatchInfo']);
-    });
-
-    Route::prefix('football/detailComp')->group(function () {
-        Route::get('/{urlComp}', [ApiController::class, 'getCompetitionDetail']);
-    });
-
-    Route::prefix('football/detailTeam')->group(function () {
-        Route::get('/{idTeam}', [ApiController::class, 'getTeamDetail']);
-        Route::get('/playerstat/{teamId}/{eventId?}', [ApiController::class, 'getPlayerStat']);
-        Route::get('/squad/{id}', [ApiController::class, 'getTeamSquad']);
-        Route::get('/stat/{teamId}/{eventId?}', [ApiController::class, 'getTeamStat']);
-        Route::get('/table/{id}', [ApiController::class, 'getTeamTable']);
-        Route::get('/news/{idTeam}', [ApiController::class, 'getTeamNews']);
-    });
-});
+// Route::prefix('api')->group(function () {
+    
+// });
 
 
 Route::get('/dashboard', function () {
